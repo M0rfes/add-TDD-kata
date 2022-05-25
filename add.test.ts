5cf8ff238a5ab6ae94fd26690ec5598a60f42808
M0rfes <fahimkhan20148@gmail.com>
2022-05-25 16:24:48 +0530

    delimiter working

--- add.test.ts
+++ add.test.ts
@@ -12,4 +12,8 @@ describe("testing add function", () => {
   it("should work with , and /\n", () => {
     expect(add("1\n2,3")).toBe(6);
   });
+
+  it("should let user define delimiter between '//' and '/n'", () => {
+    expect(add("//;\n1;2")).toBe(3);
+  });
 });
--- add.ts
+++ add.ts
@@ -1,4 +1,15 @@
-export const add = (expression: string, delimiter = ","): number => {
+export const add = (expression: string): number => {
+  let delimiter = ",";
+  let numbers = expression;
+  if (expression.startsWith("//")) {
+    const delimiterIndex = expression.indexOf("\n");
+    delimiter = expression.substring(2, delimiterIndex);
+    numbers = expression.substring(delimiterIndex + 1);
+  }
+  return _add(numbers, delimiter);
+};
+
+const _add = (expression: string, delimiter: string): number => {
   if (expression === "") {
     return 0;
   }
@@ -7,7 +18,7 @@ export const add = (expression: string, delimiter = ","): number => {
     return Number(numbers[0]);
   }
   return numbers.reduce((acc, curr) => {
-    const parcedCurr = add(curr, "\n");
+    const parcedCurr = _add(curr, "\n");
     return acc + parcedCurr;
   }, 0);
 };
