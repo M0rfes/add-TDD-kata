92ded679e3964e350cebe5cd1b51f775745183e2
M0rfes <fahimkhan20148@gmail.com>
2022-05-25 16:06:22 +0530

    2nd test pass

--- add.test.ts
+++ add.test.ts
@@ -4,4 +4,8 @@ describe("testing add function", () => {
   it("should add numbers in comma separated list", () => {
     expect(add("1,2,3")).toBe(6);
   });
+
+  it("should return 0 for empty string", () => {
+    expect(add("")).toBe(0);
+  });
 });
--- add.ts
+++ add.ts
@@ -1,3 +1,6 @@
 export const add = (expression: string): number => {
+  if (expression === "") {
+    return 0;
+  }
   return expression.split(",").reduce((acc, curr) => acc + parseInt(curr), 0);
 };
