54aed5dcfcd688af745c57f9ee21642b46c8880a
M0rfes <fahimkhan20148@gmail.com>
2022-06-02 19:01:21 +0530

    seperated parcer and expression out

--- add.test.ts
+++ add.test.ts
@@ -1,25 +1,49 @@
-import { add } from "./add";
+import { add, EmptyStringError, multiply, parcer, reduce } from "./add";
 
 describe("testing add function", () => {
-  it("should add numbers in comma separated list", () => {
-    expect(add("1,2,3")).toBe(6);
+  let logger: (number: number) => any;
+
+  beforeEach(() => {
+    logger = jest.fn();
   });
 
-  it("should return 0 for empty string", () => {
-    expect(add("")).toBe(0);
+  it("should reduce the number list with give call back and initial value", () => {
+    const numbers = [1, 2, 3, 4, 5];
+    const initialValue = 0;
+    const cb = (a: number, b: number) => a + b;
+    const ans = reduce(numbers, cb, initialValue);
+    expect(ans).toBe(15);
   });
 
-  it("should work with , and /\n", () => {
-    expect(add("1\n2,3")).toBe(6);
+  it("should return initial value for empty list", () => {
+    const numbers = [] as number[];
+    const initialValue = 0;
+    const cb = (a: number, b: number) => a + b;
+    const ans = reduce(numbers, cb, initialValue);
+    expect(ans).toBe(0);
   });
 
-  it("should let user define delimiter between '//' and '/n'", () => {
-    expect(add("//;\n1;2")).toBe(3);
+  it("should should return number list for 1,2,3,4", () => {
+    expect(parcer("1,2,3,4")).toEqual([1, 2, 3, 4]);
+  });
+
+  it("should should return number list for 1,2\n3\n4,5", () => {
+    expect(parcer("1,2\n3\n4,5")).toEqual([1, 2, 3, 4, 5]);
+  });
+
+  it("should use char in between // and \n as delimiter", () => {
+    expect(parcer("//;\n1;2;3")).toEqual([1, 2, 3]);
   });
 
   it("should fail for negative numbers", () => {
-    const t = () => add("-1,2,3,-4");
+    const t = () => parcer("-1,2,3,-4");
     expect(t).toThrow(Error);
     expect(t).toThrow("negatives not allowed: -1,-4");
+  });
+
+  it("should throw for empty string for empty string", () => {
+    const t = () => parcer("");
+    expect(t).toThrow(EmptyStringError);
+    expect(t).toThrow("expression is empty");
   });
 });
--- add.ts
+++ add.ts
@@ -1,28 +1,77 @@
-export const add = (expression: string): number => {
+export const add = (
+  expression: string,
+  logger: (number: number) => any
+): number => {
+  try {
+    const numbers = parcer(expression);
+    const ans = reduce(numbers, simpleAdd, 0);
+    logger(ans);
+    return ans;
+  } catch (error) {
+    if (error instanceof EmptyStringError) {
+      logger(0);
+      return 0;
+    } else {
+      throw error;
+    }
+  }
+};
+
+export const multiply = (
+  expression: string,
+  logger: (number: number) => any
+): number => {
+  try {
+    const numbers = parcer(expression);
+    const ans = reduce(numbers, simpleProduct, 1);
+    logger(ans);
+    return ans;
+  } catch (error) {
+    if (error instanceof EmptyStringError) {
+      logger(0);
+      return 0;
+    } else {
+      throw error;
+    }
+  }
+};
+
+const simpleAdd = (a: number, b: number) => a + b;
+
+const simpleProduct = (a: number, b: number) => a * b;
+
+export const parcer = (
+  expression: string,
+  defaultDelimiter = ","
+): number[] => {
+  if (!expression) {
+    throw new EmptyStringError("expression is empty");
+  }
   const negativeNumbers = expression.match(/\-[0-9]{1,}/g);
   if (negativeNumbers?.length) {
     throw new Error("negatives not allowed: " + negativeNumbers?.join(","));
   }
-  let delimiter = ",";
+  let delimiter = defaultDelimiter;
   let numbers = expression;
   if (expression.startsWith("//")) {
     const delimiterIndex = expression.indexOf("\n");
     delimiter = expression.substring(2, delimiterIndex);
     numbers = expression.substring(delimiterIndex + 1);
   }
-  return _add(numbers, delimiter);
+  const numberArray = numbers.split(delimiter).map((n) => {
+    if (n.match("\n")) {
+      return n.split("\n").map((num) => Number(num.replace(/\_/g, "")));
+    } else {
+      return Number(n.replace(/\_/g, ""));
+    }
+  });
+  return numberArray.flat();
 };
 
-const _add = (expression: string, delimiter: string): number => {
-  if (expression === "") {
-    return 0;
-  }
-  const numbers = expression.split(delimiter);
-  if (numbers.length === 1) {
-    return Number(numbers[0]);
-  }
-  return numbers.reduce((acc, curr) => {
-    const parcedCurr = _add(curr, "\n");
-    return acc + parcedCurr;
-  }, 0);
-};
+export const reduce = (
+  numbers: number[],
+  cb: (a: number, b: number) => number,
+  initialValue: number
+) => numbers.reduce((acc, curr) => cb(acc, curr), initialValue);
+
+export class EmptyStringError extends Error {}
